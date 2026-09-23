9cbb2bfa7d3db1436840f187b9aff314ba2f3a71
Yevhenii Zakharchenko <zahgenya@gmail.com>
2026-09-23 13:28:12 +0000

    add: grant all required privileges to app user (before that i fixed it live, now applying to config). Added missing debug user.

--- init_db.sql
+++ init_db.sql
@@ -29,7 +29,10 @@ GRANT ALL ON *.* TO 'vt_dba'@'localhost';
 GRANT GRANT OPTION ON *.* TO 'vt_dba'@'localhost';
 
 CREATE USER 'vt_app'@'localhost';
-GRANT SELECT, SHOW DATABASES, PROCESS ON *.* TO 'vt_app'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER ON *.* TO `vt_app`@`localhost`;
+
+CREATE USER 'vt_appdebug'@'localhost';
+GRANT SELECT, SHOW DATABASES, PROCESS ON *.* TO 'vt_appdebug'@'localhost';
 
 CREATE USER 'vt_allprivs'@'localhost';
 GRANT ALL ON *.* TO 'vt_allprivs'@'localhost';
